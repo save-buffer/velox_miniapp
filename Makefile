c67a2f57a2adb8ccf23371d60f349b86096baa2d
Sasha Krassovsky <krassovskysasha@gmail.com>
2022-11-03 13:03:00 -0700

    Add tpch data and json support

--- Makefile
+++ Makefile
@@ -9,14 +9,17 @@ PROTOBUF_SRC = ${VELOX_BIN}/_deps/protobuf-src/src
 XXHASH_SRC = ${VELOX_ROOT}/velox/external/xxhash
 
 INCLUDES = -I${VELOX_SRC} -I${FOLLY_SRC} -I${FOLLY_SRC}/../folly-build -I${XSIMD_SRC} -I${GTEST_SRC} -I${PROTOBUF_SRC} -I${VELOX_GEN} -I${XXHASH_SRC}
-FLAGS = -O3 -std=gnu++17 -mavx -pthread
+FLAGS = -O3 -std=gnu++17 -mavx -pthread -ggdb
 
 VELOX_LIB = $(shell find ${VELOX_BIN} -name "libvelox*.a")
-FOLLY_LIB = ${FOLLY_SRC}/../folly-build/libfolly.a
-PROTO_LIB = ${VELOX_BIN}/_deps/protobuf-build/libprotobuf.a ${VELOX_BIN}/_deps/protobuf-build/libprotoc.a
+FOLLY_LIB = $(shell find ${VELOX_BIN} -name "libfolly.a")
+PROTO_LIB = $(shell find ${VELOX_BIN} -name "libprotobuf.a")
+MD5_LIB = $(shell find ${VELOX_BIN} -name "libmd5.a")	
+DUCKDB_LIB = $(shell find ${VELOX_BIN} -name "libduckdb.a") $(shell find ${VELOX_BIN} -name "libtpch_extension.a") $(shell find ${VELOX_BIN} -name "libdbgen.a")
+ARROW_LIB = ${VELOX_BIN}/third_party/arrow_ep/install/lib64/*.a
 
-VELOX_ARCHIVES = ${VELOX_LIB} ${FOLLY_LIB} ${PROTO_LIB}
-SHARED_LIBRARIES = -lglog -lgflags -lpthread -lfmt -ldl -levent -ldouble-conversion -lre2 -lboost_atomic -lboost_program_options -lboost_context -lboost_filesystem -lboost_regex -lboost_thread -lboost_system -lboost_date_time -L/usr/lib64 -lsnappy -llz4 -lzstd -lz
+VELOX_ARCHIVES = ${VELOX_LIB} ${FOLLY_LIB} ${PROTO_LIB} ${MD5_LIB} ${DUCKDB_LIB} ${ARROW_LIB}
+SHARED_LIBRARIES = -lglog -lgflags -lpthread -lfmt -ldl -levent -ldouble-conversion -lre2 -lboost_atomic -lboost_program_options -lboost_context -lboost_filesystem -lboost_regex -lboost_thread -lboost_system -lboost_date_time -L/usr/lib64 -lsnappy -llz4 -lzstd -lz -levent_openssl -lcrypto
 
 .DEFAULT_GOAL = test
 
--- from_substrait.cpp
+++ from_substrait.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <string_view>
+
+#include <google/protobuf/util/json_util.h>
 
 #include <folly/init/Init.h>
 #include "velox/exec/Task.h"
@@ -7,12 +10,60 @@
 #include "velox/core/QueryCtx.h"
 #include "velox/common/memory/Memory.h"
 #include "velox/substrait/SubstraitToVeloxPlan.h"
+#include "velox/common/file/FileSystems.h"
+#include "velox/connectors/hive/HiveConnector.h"
+#include "velox/connectors/hive/HiveConnectorSplit.h"
+#include "velox/dwio/parquet/RegisterParquetReader.h"
+#include "velox/functions/prestosql/registration/RegistrationFunctions.h"
+
+constexpr const char *Usage =
+    "\tfrom_substrait <filename>\n"
+    "\tFile type is inferred from extension. If it ends in .pb or .proto, it's a protobuf.\n"
+    "\tIf it ends in .json, it's a json. If neither, it's an error.\n";
+
+// In C++20 we'd just use string_view::ends_with...
+bool MatchesExtension(std::string_view filename, std::string_view extension)
+{
+    if(extension.size() > filename.size())
+        return false;
+    return std::equal(extension.rbegin(), extension.rend(), filename.rbegin());
+}
 
 int main(int argc, char **argv)
 {
     folly::init(&argc, &argv);
 
+    if(argc != 2)
+    {
+        std::cerr << "Usage: " << Usage;
+        return 1;
+    }
+
+    std::string_view filename(argv[1]);
+    bool is_json = MatchesExtension(filename, ".json");
+    bool is_pb = MatchesExtension(filename, ".pb") || MatchesExtension(filename, ".proto");
+ 
+    if(!is_json && !is_pb)
+    {
+        std::cerr << "Unrecognized file extension for file " << filename << '\n';
+        return 1;
+    }
+
     substrait::Plan substrait_plan;
+
+    if(is_json)
+    {
+        std::ifstream stream(argv[1]);
+        std::stringstream json;
+        json << stream.rdbuf();
+        auto status = google::protobuf::util::JsonStringToMessage(json.str(), &substrait_plan);
+        if(!status.ok())
+        {
+            std::cerr << "Failed to parse JSON from file " << filename << " as json: " << status.message() << '\n';
+            return 1;
+        }
+    }
+    else if (is_pb)
     {
         std::ifstream stream(argv[1], std::ios::binary);
         if(!substrait_plan.ParseFromIstream(&stream))
@@ -22,16 +73,47 @@ int main(int argc, char **argv)
         }
     }
 
-    auto ctx = std::make_shared<facebook::velox::core::QueryCtx>();
+    std::shared_ptr<facebook::velox::core::QueryCtx> ctx = facebook::velox::core::QueryCtx::createForTest();
     facebook::velox::substrait::SubstraitVeloxPlanConverter converter(ctx->pool());
-    auto plan_node = converter.toVeloxPlan(substrait_plan);
+    std::shared_ptr<const facebook::velox::core::PlanNode> plan_node = converter.toVeloxPlan(substrait_plan);
+    std::cout << "Plan:\n" << plan_node->toString(true, true) << "\n";
     facebook::velox::core::PlanFragment fragment(std::move(plan_node));
 
-    facebook::velox::exec::Task task("plan", fragment, /*destination partition=*/0, ctx);
-    while(auto result = task.next())
+    // Zeal of wexort! Galeron's Abyssal Carnesphere! Zuckerberg's Factory of Hive Connectors! - Carl, the Invoker
+    auto hive_connector = facebook::velox::connector::getConnectorFactory(
+        facebook::velox::connector::hive::HiveConnectorFactory::kHiveConnectorName)->newConnector("test-hive", nullptr);
+    facebook::velox::connector::registerConnector(hive_connector);
+    facebook::velox::filesystems::registerLocalFileSystem();
+    facebook::velox::parquet::registerParquetReaderFactory();
+    facebook::velox::functions::prestosql::registerAllScalarFunctions();
+
+    auto task = std::make_shared<facebook::velox::exec::Task>("plan", fragment, /*destination partition=*/0, ctx);
+    for(const auto &split_info : converter.splitInfos())
     {
-        for(auto i = 0; i < result->size(); i++)
-            std::cout << result->toString(i);
+        for(int i = 0; i < split_info.second->paths.size(); i++)
+        {
+            std::cout << split_info.second->paths[i] << ", " << split_info.second->starts[i] << ", " << split_info.second->lengths[i] << "\n";
+            auto connector_split = std::make_shared<facebook::velox::connector::hive::HiveConnectorSplit>(
+                "test-hive" /*connectorId*/,
+                split_info.second->paths[i],
+                split_info.second->format,
+                split_info.second->starts[i],
+                split_info.second->lengths[i]);
+            facebook::velox::exec::Split split;
+            split.connectorSplit = std::move(connector_split);
+            task->addSplit(split_info.first, std::move(split));
+        }
+        task->noMoreSplits(split_info.first);
     }
+
+    int num_batches_received = 0;
+    while(auto result = task->next())
+    {
+        std::cout << "Got a result of size " << result->size() << ":\n";
+        for(auto i = 0; i < result->size(); i++)
+            std::cout << "\t" << result->toString(i) << "\n";
+        num_batches_received++;
+    }
+    std::cout << "Finished with " << num_batches_received << " batches received\n";
     return 0;
 }
